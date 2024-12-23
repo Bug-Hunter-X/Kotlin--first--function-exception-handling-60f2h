f0ba47fcb6da5b4467d4e64f834e07f295f4f457
BugHunter-X <askzenai@gmail.com>
2024-12-23 15:26:05 +0100

    Add bug.kt file

---
+++ bug.kt
@@ -0,0 +1,10 @@
+```kotlin
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5)
+    val doubledList = list.map { it * 2 }
+    println(doubledList)
+    //This will throw an exception
+    val firstEven = doubledList.first { it % 2 == 0 }
+    println(firstEven)
+}
+```
